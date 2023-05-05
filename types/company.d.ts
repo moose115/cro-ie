bb4fca78465fd6f18c61e36fa912de109954a065
Mustafa <mustafaucuncu@onet.pl>
2023-05-04 19:25:37 -0600

    Create company type

---
+++ types/company.d.ts
@@ -0,0 +1,19 @@
+export type Company = {
+  companyNum: number;
+  companyBusInd: string;
+  companyName: string;
+  companyAddr1: string;
+  companyAddr2: string;
+  companyAddr3: string;
+  companyAddr4: string;
+  companyRegDate: string;
+  companyStatusDesc: string;
+  companyStatusDate: string;
+  lastArDate: string;
+  nextArDate: string;
+  lastAccdate: string;
+  companyTypeCode: number;
+  companyStatusCode: number;
+  placeOfBusiness: string;
+  eircode: string;
+};
